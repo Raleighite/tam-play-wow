5b271c1a0650da0056eac2fb72ba1ccb6bd99d82
Trav <tjbailey10@gmail.com>
2023-06-23 23:46:36 -0400

    Added critical comment to app.py

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "No, Tam has not played WoW yet."
+
+    """In May 2023 Tam promissed his friends he would play World of Warcraft for 3 months starting at some point during the summer of 2023. This website tracks if Tam has played WoW yet, and reminds him of his commitment. 
+    """
