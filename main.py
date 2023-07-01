9a4545c0c62972aadfe72bd44ea1ec06deaba1c6
andrew <andrew@andrewsmith.org>
2023-07-01 14:02:46 -0400

    Fix typo

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return "No, Tam has not played WoW yet."
 
-    """In May 2023 Tam promissed his friends he would play World of Warcraft for 3 months starting at some point during the summer of 2023. This website tracks if Tam has played WoW yet, and reminds him of his commitment. 
+    """In May 2023 Tam promised his friends he would play World of Warcraft for 3 months starting at some point during the summer of 2023. This website tracks if Tam has played WoW yet, and reminds him of his commitment. 
     """
     
 if __name__ == '__main__':
