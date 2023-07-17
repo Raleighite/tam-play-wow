06f0c7b796d465053664bed8e8c58fc4ec3726f3
Trav <tjbailey10@gmail.com>
2023-07-17 08:34:56 -0400

    status update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "No, Tam has not played WoW yet."
+    return "Yes, Tam has played WoW very briefly, he still owes his friends 3 months of WoW playtime."
 
     """In May 2023 Tam promissed his friends he would play World of Warcraft for 3 months starting at some point during the summer of 2023. This website tracks if Tam has played WoW yet, and reminds him of his commitment. 
     """
