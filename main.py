c0409f0b4386ee182a9db25ae8709f65efe7f235
Raleighite <46750225+Raleighite@users.noreply.github.com>
2026-07-05 08:01:45 -0400

    Fix unreachable docstring: move before return statement

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def home():
-    return "Yes, Tam has played WoW, but, he hasn't leveled a character to 70 yet."
-
     """In May 2023 Tam promised his friends he would play World of Warcraft for 3 months starting at some point during the summer of 2023. This website tracks if Tam has played WoW yet, and reminds him of his commitment. 
     """
+    return "Yes, Tam has played WoW, but, he hasn't leveled a character to 70 yet."
     
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv('PORT', default=5000))
